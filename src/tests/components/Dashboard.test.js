4a4f6197518fbcc26519a223c0fce6dd8abcd301
rachelhs <rachelsmith400@hotmail.co.uk>
2020-07-02 13:11:00 +0100

    delete unneccesary files

--- src/tests/components/Dashboard.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ExpenseDashboardPage from '../../components/Dashboard';
-import toJSON from 'enzyme-to-json';
-
-test('should render Dashboard page', () => {
-    const wrapper = shallow(<ExpenseDashboardPage />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
-});
--- src/tests/components/NotFound.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import NotFound from '../../components/NotFound';
-import toJSON from 'enzyme-to-json';
-
-test('should render Not Found page', () => {
-    const wrapper = shallow(<NotFound />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
-});
