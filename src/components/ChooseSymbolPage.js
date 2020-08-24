7c3cd9b7aae7abb54004f992e2c2554d57d3d995
rachelhs <rachelsmith400@hotmail.co.uk>
2020-08-24 11:27:36 +0100

    choose symbol page redirects to edit/entryid

--- src/components/ChooseSymbolPage.js
+++ src/components/ChooseSymbolPage.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import EntryForm from './EntryForm';
 import { connect } from 'react-redux';
-import { startAddEntry } from '../actions/entries'
+import { startAddEntry, startEditEntry } from '../actions/entries'
 import BackgroundAnimation from './BackgroundAnimation'
 import ForegroundAnimation from './ForegroundAnimation'
+const firebase = require('firebase/app');
+require('firebase/auth');
+import database from '../firebase/firebase';
 
 export class ChooseSymbolPage extends React.Component {
     onSubmit = (entry) => {
+        //create new entry
         this.props.startAddEntry(entry);
-        this.props.history.push('/edit/-MFAEOzkAZxz_n2FpnQl');
+        //get current user
+        const user = firebase.auth().currentUser;
+        const uid = user.uid;
+        let name = '';
+        //get id for the new entry
+        database.ref(`users/${uid}/entries`).orderByChild('createdAt').limitToLast(1).on('child_added', function(snapshot) {
+            name = snapshot.key;
+        })
+        //go to page for most recent entry
+        this.props.history.push(`edit/${name}`);
     };
+
     render() {
         return (
             <div>
@@ -30,10 +44,18 @@ export class ChooseSymbolPage extends React.Component {
     }
 }
 
+const mapStateToProps = (state, props) => {
+    return {
+        entry: state.entries.find((entry) => entry.id === props.match.params.id)
+    };
+};
+
 const mapDispatchToProps = (dispatch) => ({
 
-    startAddEntry: (entry) => dispatch(startAddEntry(entry))
+    startAddEntry: (entry) => dispatch(startAddEntry(entry)),
+    startEditEntry: (id, entry) => dispatch(startEditEntry(id, entry))
+
 
 });
 
-export default connect(undefined, mapDispatchToProps)(ChooseSymbolPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ChooseSymbolPage);
