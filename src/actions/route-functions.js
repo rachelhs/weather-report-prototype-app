5def7ecb788cbbfe5e98c2a8e0787ce3eab4441b
EllieForeman <foreman.eleanor@gmail.com>
2020-09-03 14:16:50 +0100

    finished - callback changes state from functon call

--- src/actions/route-functions.js
+++ src/actions/route-functions.js
@@ -1,157 +1,23 @@
-import React, { useState } from 'react';
-import BackgroundAnimation from '../components/Animations/BackgroundAnimation'
-import ForegroundAnimation from '../components/Animations/ForegroundAnimation'
+// FILE CONTAINING FUNCTIONS WHICH ARE SHARED ACROSS PATHS
 const firebase = require('firebase/app');
 import database from '../firebase/firebase';
 
 
-export async function isLongerThanThreeDays(n) {
-    // get timestamp for now
-    const now = Date.now();
-    // get timestamp for 3 days ago (259200000 milliseconds)
-    const threeDaysAgo = now - 259200000;
-    console.log('3 days', threeDaysAgo)
-    // has user updated mood in the previous 3 days? - retrieve entry before the current one
-    const uid = n.uid;
-    // return two most recent entries
-    await database.ref(`users/${uid}/entries`)
-    .orderByChild('createdAt')
-    .limitToLast(2)
-    .on('value', (snapshot) => {
+//is longer than 3 days function
+export function isLongerThanThreeDays(cb) {
+    const now = Date.now(); // get timestamp for now
+    const threeDaysAgo = now - 259200000; // get timestamp for 3 days ago (259200000 milliseconds)
+    const uid = firebase.auth().currentUser.uid;
+    database.ref(`users/${uid}/entries`)
+    .orderByChild('createdAt') //order by createAt value
+    .limitToLast(2) // return two most recent entries
+    .once('value', (snapshot) => {
         snapshot.forEach((child) => {
             let time = child.val().createdAt;
-            let isLonger = '';
-            console.log(time);
             if (time < threeDaysAgo) {
-                console.log('true')
-                //return Promise.resolve("less than threeDaysAgo");
-                isLonger = "less than"
-            }
-            else {
-                console.log('false')
-                //return Promise.resolve("more than threeDaysAgo");
-                isLonger = "more than"
+              cb(true)
+              return
             }
         })
     })
-    let result = await isLonger;
-    return result;
-}
-
-
-export function doSomething(num) {
-    if (num > 1) {
-        return "bigger than one"
-    } else {
-        return "smaller than one"
-    }
-   }
-   
-
-export class AnimationsCombined extends React.Component {
-    render() {
-        return (
-            <span>
-                <div className='background-anim'>
-                    <BackgroundAnimation />
-                </div>
-                <div className='foreground-anim'>
-                    <ForegroundAnimation />
-                </div>
-            </span>
-        )
-    }
-}
-
-export class LowAcknowledgement extends React.Component {
-    render() {
-        let script = require('../data/script.json');
-        return (
-            <h1>{script[0].low[1]}</h1>
-        )
-    }
-}
-
-export class FadeOut extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {visible:true}
-    }
-  
-    componentWillReceiveProps(nextProps) {
-      // reset the timer if children are changed
-      if (nextProps.children !== this.props.children) {
-        this.setTimer();
-        this.setState({visible: true});
-      }
-    }
-  
-    componentDidMount() {
-      this.setTimer();
-    }
-  
-    setTimer() {
-      // clear any existing timer
-      if (this._timer != null) {
-        clearTimeout(this._timer)
-      }
-  
-      // hide after `delay` milliseconds
-      this._timer = setTimeout(function(){
-        this.setState({visible: false});
-        this._timer = null;
-      }.bind(this), this.props.delay);
-    }
-  
-    componentWillUnmount() {
-      clearTimeout(this._timer);
-    }
-  
-    render() {
-      return this.state.visible
-        ? <div>{this.props.children}</div>
-        : <div className="fadeOut">{this.props.children}</div>;
-    }
-}
-
-export class FadeIn extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {visible:true}
-    }
-  
-    componentWillReceiveProps(nextProps) {
-      // reset the timer if children are changed
-      if (nextProps.children !== this.props.children) {
-        this.setTimer();
-        this.setState({visible: true});
-      }
-    }
-  
-    componentDidMount() {
-      this.setTimer();
-    }
-  
-    setTimer() {
-      // clear any existing timer
-      if (this._timer != null) {
-        clearTimeout(this._timer)
-      }
-  
-      // hide after `delay` milliseconds
-      this._timer = setTimeout(function(){
-        this.setState({visible: false});
-        this._timer = null;
-      }.bind(this), this.props.delay);
-    }
-  
-    componentWillUnmount() {
-      clearTimeout(this._timer);
-    }
-  
-    render() {
-      return this.state.visible
-        ? <span />
-        : <div className="fadeIn">{this.props.children}</div>;
-    }
 }
--- src/components/Routes/MediumLowRoute.js
+++ src/components/Routes/MediumLowRoute.js
@@ -1,28 +1,50 @@
 import React from 'react';
-import { LowAcknowledgement, AnimationsCombined, FadeOut, FadeIn, doSomething, isLongerThanThreeDays} from '../../actions/route-functions'
-import { IsLonger } from '../FunctionalRouteComponents/IsLonger'
-
-const firebase = require('firebase/app');
-require('firebase/auth');
-
+import { LowAcknowledgement, AnimationsCombined, FadeOut, FadeIn } from '../SharedComponents/SharedComponents';
+import { isLongerThanThreeDays } from '../../actions/route-functions';
 
 class MediumLowRoute extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {LongerThanThree: false}
+        this.state = {
+            LongerThanThree: false
+        }
       }
 
-    componentWillMount() {
-        // var x = isLongerThanThreeDays(firebase.auth().currentUser)
-        // console.log('x', x)
-        var number = doSomething(1)
-        console.log('variable', number)
+    componentDidMount = () => {
+      isLongerThanThreeDays(res => {
+        this.setState({
+            LongerThanThree: res
+        })
+        console.log('result', res)
+      });
+    }
+
+    onHowLongAnswered = () => {
+        this.setState({ HowLongAnswered: true });
+        console.log('clicked next', this.state.HowLongAnswered)
     }
 
 
     render() {
-        let user = firebase.auth().currentUser
+        console.log('state', this.state.LongerThanThree)
+        let script = require('../../data/script.json');
+        let text;
+        console.log('longer than three', this.state.LongerThanThree)
+
+        if (this.state.LongerThanThree) {
+            text =   
+            <div className='info-box'>
+                <h1>{script[0].low[2]}</h1>
+            </div>
+        };
+        if (!this.state.LongerThanThree || this.state.HowLongAnswered){
+            text = 
+            <div className='info-box'>
+                <h1>{script[0].low[3]}</h1>
+                <button className='next-button' onClick={this.onHowLongAnswered}>next</button>
+            </div>
+        }
         return (
             <div className='background-box'>
                 <AnimationsCombined />
@@ -30,14 +52,11 @@ class MediumLowRoute extends React.Component {
                     <FadeOut delay={500}>
                         <LowAcknowledgement />
                     </FadeOut>
-                    <IsLonger user={user} />
-                    {console.log(this.state.LongerThanThree)}
-                    {/* <FadeIn delay={5000}>
-                        <h1>What had led you to feeling this way?</h1>
-                    </FadeIn> */}
-                    {/* <div className='info-box-button'>
-                        <button className='next-button' onClick={this.onNext}>next</button>
-                    </div> */}
+                    <FadeIn delay={5000}>
+                        <div>
+                            {text}
+                        </div>
+                    </FadeIn>
                 </div>
             </div>
         );
