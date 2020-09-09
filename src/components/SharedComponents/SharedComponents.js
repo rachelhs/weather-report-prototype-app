8d06be9021407ce91e1f929430d5297389295206
rachelhs <rachelsmith400@hotmail.co.uk>
2020-09-09 13:11:08 +0100

    add modals to first aid kit

--- src/components/SharedComponents/SharedComponents.js
+++ src/components/SharedComponents/SharedComponents.js
@@ -7,12 +7,14 @@ const firebase = require('firebase/app');
 require('firebase/auth');
 import database from '../../firebase/firebase';
 import { CSSTransition } from 'react-transition-group';
+import ReactModal from 'react-modal';
+import { SetExercises } from '../Exercises/SetExercises';
 
 // I’m sorry you are feeling like this
 export class LowAcknowledgement extends React.Component {
     render() {
         return (
-            <h1 className='info-box-title'>{ data[3].shared.acknowledge }</h1>
+            <h1 className='info-box-title'>{data[3].shared.acknowledge}</h1>
         )
     }
 }
@@ -21,7 +23,7 @@ export class LowAcknowledgement extends React.Component {
 export class VeryLowAcknowledgement extends React.Component {
     render() {
         return (
-            <h1 className='info-box-title'>{ data[3].shared.lowerAcknowledge }</h1>
+            <h1 className='info-box-title'>{data[3].shared.lowerAcknowledge}</h1>
         )
     }
 }
@@ -36,21 +38,21 @@ export class HowLongHaveYouFeltLikeThis extends React.Component {
         database.ref(`users/${uid}/entries`).orderByChild('createdAt').limitToLast(1).on('child_added', (snapshot) => {
             name = snapshot.key;
             database.ref(`users/${uid}/entries/${name}`).update({
-            howLong: howLong
+                howLong: howLong
             })
         })
     };
     render() {
         return (
-          <div>
-            <h1 className='info-box-title'>{ data[3].shared.howLong }</h1>
-            <div className='button-container'>
-                <button className='next-button' onClick={() => this.onHowLong('today')} onClick={this.props.buttonClick}>today</button>
-                <button className='next-button' onClick={() => this.onHowLong('a few days')} onClick={this.props.buttonClick}>a few days</button>
-                <button className='next-button' onClick={() => this.onHowLong('a week')} onClick={this.props.buttonClick}>a week</button>
-                <button className='next-button' onClick={() => this.onHowLong('longer')} onClick={this.props.buttonClick}>longer</button>
+            <div>
+                <h1 className='info-box-title'>{data[3].shared.howLong}</h1>
+                <div className='button-container'>
+                    <button className='next-button' onClick={() => this.onHowLong('today')} onClick={this.props.buttonClick}>today</button>
+                    <button className='next-button' onClick={() => this.onHowLong('a few days')} onClick={this.props.buttonClick}>a few days</button>
+                    <button className='next-button' onClick={() => this.onHowLong('a week')} onClick={this.props.buttonClick}>a week</button>
+                    <button className='next-button' onClick={() => this.onHowLong('longer')} onClick={this.props.buttonClick}>longer</button>
+                </div>
             </div>
-          </div>
         )
     }
 }
@@ -60,7 +62,7 @@ export class ReasonForFeelings extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='info-box-title'>{ data[3].shared.reason }</h1>
+                <h1 className='info-box-title'>{data[3].shared.reason}</h1>
                 <div className='button-container'>
                     <button className='next-button' onClick={(e) => this.props.onClick(true)}>Yes</button>
                     <button className='next-button' onClick={(e) => this.props.onClick(false)}>No</button>
@@ -75,7 +77,7 @@ export class AnotherExerciseQuestion extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='info-box-title'>{ data[3].shared.anotherExercise }</h1>
+                <h1 className='info-box-title'>{data[3].shared.anotherExercise}</h1>
                 <div className='button-container'>
                     <button className='next-button' onClick={(e) => this.props.onClick(true)}>Yes</button>
                     <button className='next-button' onClick={(e) => this.props.onClick(false)}>No</button>
@@ -90,7 +92,7 @@ export class SafeQuestion extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='info-box-title'>{ data[3].shared.safeQuestion }</h1>
+                <h1 className='info-box-title'>{data[3].shared.safeQuestion}</h1>
                 <div className='button-container'>
                     <button className='next-button' onClick={(e) => this.props.onClick(true)}>Yes</button>
                     <button className='next-button' onClick={(e) => this.props.onClick(false)}>No</button>
@@ -105,9 +107,9 @@ export class FeedbackStatement extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='info-box-title'>{ data[3].shared.feedbackStatement }</h1>
+                <h1 className='info-box-title'>{data[3].shared.feedbackStatement}</h1>
                 <div className='button-container'>
-                <button className='next-button' onClick={(e) => this.props.onClick(true)}>home</button>
+                    <button className='next-button' onClick={(e) => this.props.onClick(true)}>home</button>
                 </div>
             </div>
         )
@@ -148,7 +150,7 @@ export class ReasonForFeelingsInput extends React.Component {
                     <input className='free-form-input' type="text" value={this.state.value} onChange={this.handleNoteChange} />
                     <button className='next-button free-form-submit' onClick={this.props.buttonClick}>Submit</button>
                 </form>
-            </div> 
+            </div>
         )
     }
 }
@@ -176,12 +178,12 @@ export class PositiveThingQuestion extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='info-box-title'>{ data[4].mediumLow.questions.posThing }</h1>
+                <h1 className='info-box-title'>{data[4].mediumLow.questions.posThing}</h1>
                 <form className='button-container' onSubmit={this.handlePosSubmit}>
                     <input className='free-form-input' type="text" value={this.state.value} onChange={this.handlePosChange} />
                     <button className='next-button free-form-submit' onClick={this.props.buttonClick}>Submit</button>
                 </form>
-            </div> 
+            </div>
         )
     }
 }
@@ -210,12 +212,12 @@ export class FriendsLikeQuestion extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='info-box-title'>{ data[4].mediumLow.questions.friendLike }</h1>
+                <h1 className='info-box-title'>{data[4].mediumLow.questions.friendLike}</h1>
                 <form className='button-container' onSubmit={this.handleFriendsSubmit}>
                     <input className='free-form-input' type="text" value={this.state.value} onChange={this.handleFriendsChange} />
                     <button className='next-button free-form-submit' onClick={this.props.buttonClick}>Submit</button>
                 </form>
-            </div> 
+            </div>
         )
     }
 }
@@ -244,12 +246,12 @@ export class HelpedCopeQuestion extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='info-box-title'>{ data[5].low.questions.helpedCope }</h1>
+                <h1 className='info-box-title'>{data[5].low.questions.helpedCope}</h1>
                 <form className='button-container' onSubmit={this.handleCopedSubmit}>
                     <input className='free-form-input' type="text" value={this.state.value} onChange={this.handleCopedChange} />
                     <button className='next-button free-form-submit' onClick={this.props.buttonClick}>Submit</button>
                 </form>
-            </div> 
+            </div>
         )
     }
 }
@@ -259,33 +261,81 @@ export class FirstAidKit extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {showQuestions: true}
+        this.state = {
+            showQuestions: true,
+            showModalGrounding: false,
+            showModal999: false,
+            content: '',
+            contentText: ''
+        }
+
+        this.handleOpenModalGrounding = this.handleOpenModalGrounding.bind(this);
+        this.handleCloseModalGrounding = this.handleCloseModalGrounding.bind(this);
+        this.handleOpenModal999 = this.handleOpenModal999.bind(this);
+        this.handleCloseModal999 = this.handleCloseModal999.bind(this);
+    }
+
+    handleOpenModalGrounding() {
+        this.setState({ showModalGrounding: true });
+    }
+
+    handleCloseModalGrounding() {
+        this.setState({ showModalGrounding: false });
+    }
+
+    handleOpenModal999() {
+        this.setState({ showModal999: true });
+    }
+
+    handleCloseModal999() {
+        this.setState({ showModal999: false });
     }
 
     componentDidMount() {
         // set timeout for prompts
-        setTimeout( () => { this.setState({ showQuestions: false }) }, 3000)
+        setTimeout(() => { this.setState({ showQuestions: false }) }, 3000)
     }
 
-    showOptions () {
-        this.setState({ showQuestions: false,
-                        showOptions: true })
+    showOptions() {
+        this.setState({
+            showQuestions: false,
+            showOptions: true
+        })
     }
 
-    render () {
+    render() {
         return (
             <div>
-            <CSSTransition in={this.state.showQuestions} timeout={2000} classNames="fade" unmountOnExit appear onExited={() => this.showOptions()}>
-            <div><h1 className='info-box-title'>{ data[7].firstAid.questions.help }</h1><h1 className='info-box-title'>{ data[7].firstAid.questions.need }</h1></div></CSSTransition>
-            <CSSTransition in={this.state.showOptions} timeout={2000} classNames="fade" unmountOnExit>
-            <div className='button-container-vertical'>
-            <button className='next-button'>Grounding Exercise</button>
-            <button className='next-button'>Reach out to Supporters / Roots</button>
-            <button className='next-button'>Reach out to GP / Support Worker</button>
-            <button className='next-button'>Call Bristol Mental Health Crisis Team: 0300 555 0334</button>
-            <button className='next-button'>Ring 999 or go to A & E</button>
-            </div>
-            </CSSTransition>
+                <CSSTransition in={this.state.showQuestions} timeout={2000} classNames="fade" unmountOnExit appear onExited={() => this.showOptions()}>
+                    <div><h1 className='info-box-title'>{data[7].firstAid.questions.help}</h1><h1 className='info-box-title'>{data[7].firstAid.questions.need}</h1></div></CSSTransition>
+                <CSSTransition in={this.state.showOptions} timeout={2000} classNames="fade" unmountOnExit>
+                    <div className='button-container-vertical'>
+                        <button onClick={this.handleOpenModalGrounding} className='next-button'>Grounding Exercise</button>
+                        <button className='next-button'>Reach out to Supporters / Roots</button>
+                        <button className='next-button'>Reach out to GP / Support Worker</button>
+                        <button className='next-button'>Call Bristol Mental Health Crisis Team: 0300 555 0334</button>
+                        <button onClick={this.handleOpenModal999} className='next-button'>Ring 999 or go to A & E</button>
+
+                        <ReactModal 
+                        isOpen={this.state.showModalGrounding}
+                        contentLabel="Minimal Modal Example"
+                        ariaHideApp={false}
+                     >
+                       <button onClick={this.handleCloseModalGrounding}>X</button>
+                       {SetExercises('grounding')}
+                     </ReactModal>
+
+                     <ReactModal 
+                     isOpen={this.state.showModal999}
+                     contentLabel="Minimal Modal Example"
+                     ariaHideApp={false}
+                  >
+                    <button onClick={this.handleCloseModal999}>X</button>
+                    <h1 className='info-box-title'>{data[7].firstAid.options.ae}</h1>
+                  </ReactModal>
+
+                    </div>
+                </CSSTransition>
             </div>
         )
     }
