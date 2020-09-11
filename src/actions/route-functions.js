fa60576eac2461ed04af308708a72c0e31f1529d
rachelhs <rachelsmith400@hotmail.co.uk>
2020-09-11 15:15:40 +0100

    nearly finished

--- src/actions/route-functions.js
+++ src/actions/route-functions.js
@@ -8,21 +8,21 @@ export function isLongerThanThreeDays(cb) {
     const threeDaysAgo = now - 259200000; // get timestamp for 3 days ago (259200000 milliseconds)
     const uid = firebase.auth().currentUser.uid;
     database.ref(`users/${uid}/entries`)
-    .orderByChild('createdAt') //order by createAt value
-    .limitToLast(2) // return two most recent entries
-    .once('value', (snapshot) => {
-        snapshot.forEach((child) => {
-            let time = child.val().createdAt;
-            if (time < threeDaysAgo) {
-                cb(true)
-                return
-            }
-            else {
-                cb(false)
-                return
-            }
+        .orderByChild('createdAt') //order by createAt value
+        .limitToLast(2) // return two most recent entries
+        .once('value', (snapshot) => {
+            snapshot.forEach((child) => {
+                let time = child.val().createdAt;
+                if (time < threeDaysAgo) {
+                    cb(true)
+                    return
+                }
+                else {
+                    cb(false)
+                    return
+                }
+            })
         })
-    })
 }
 
 //set random question
@@ -41,6 +41,35 @@ export function chooseAnotherRandomExercise(exerciseArray, lastExercise) {
     }
     return exerciseArray;
 }
+
+// functions which checks if a user has inputed suicidal as mainWord x times in the last week
+export function expressedSuicidalRecently(cb) {
+    const uid = firebase.auth().currentUser.uid;
+    let counter = 0;
+    database.ref(`users/${uid}/entries`)
+        .limitToLast(21) // last 21 entries is max possible in a week
+        .once('value', (snapshot) => {
+            snapshot.forEach((child) => {
+                let mainWord = child.val().mainWord;
+                if (mainWord === 'suicidal') {
+                    counter += 1;
+                }
+            })
+            if(counter >= 3) {
+                cb(true)
+                return
+            }
+            else {
+                cb(false)
+                return
+            }
+        })
+}
+
+
+
+
+
 
 
 // print out a random positive statement
