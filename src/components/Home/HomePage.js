10bab3f3e5f5f86dfae0d412c90a6b749431fa70
rachelhs <rachelsmith400@hotmail.co.uk>
2020-09-22 16:46:56 +0100

    gratitude fish appears

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,36 +1,111 @@
 import React from 'react';
 import { AnimationsLayered } from '../SharedComponents/SharedComponents'
-import animatedFish from '../../animations/fish.json';
-import Lottie from 'react-lottie'
+import Animation from '../../components/Animations/Animation'
+import ReactModal from 'react-modal';
 
 class HomePage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            Fish: true
+            Fish: false,
+            showModalFish: false,
+            showModalPlant: false,
+            showModalPebble: false
         }
+        this.handleOpenModalFish = this.handleOpenModalFish.bind(this);
+        this.handleCloseModalFish = this.handleCloseModalFish.bind(this);
+        this.handleOpenModalPlant = this.handleOpenModalPlant.bind(this);
+        this.handleCloseModalPlant = this.handleCloseModalPlant.bind(this);
+        this.handleOpenModalPebble = this.handleOpenModalPebble.bind(this);
+        this.handleCloseModalPebble = this.handleCloseModalPebble.bind(this);
     }
 
-    showFish() {
-        //Math.floor(Math.random() * (max - min) ) + min;
-        const rand = Math.floor(Math.random() * (10000 - 3000) ) + 3000;
-        setTimeout( () => { this.setState({ Fish: true }) }, rand)
+    // toggles the fish at random intervals
+    // Math.random() * (max - min) + min
+    componentDidMount() {
+        let rand = Math.random() * (10000 - 3000) + 3000
+        let intervalId = setInterval(() => {
+          this.setState(prevState => {
+            if (prevState.Fish === true) {
+              return {
+                Fish: false
+              };
+            } else {
+              return {
+                Fish: true
+              };
+            }
+          });
+        }, rand);
+    
+        this.setState({
+          intervalId
+        });
+      }
+    
+      componentWillUnmount() {
+        clearInterval(this.state.intervalId);
+      }
+
+    // opens and closes the modals
+    handleOpenModalFish() {
+        this.setState({ showModalFish: true });
     }
 
-    hideFish() {
-        const rand = Math.floor(Math.random() * (10000 - 3000) ) + 3000;
-        setTimeout( () => { this.setState({ Fish: false }) }, rand)
+    handleCloseModalFish() {
+        this.setState({ showModalFish: false });
+    }
+
+    handleOpenModalPlant() {
+        this.setState({ showModalPlant: true });
+    }
+
+    handleCloseModalPlant() {
+        this.setState({ showModalPlant: false });
+    }
+
+    handleOpenModalPebble() {
+        this.setState({ showModalPebble: true });
+    }
+
+    handleCloseModalPebble() {
+        this.setState({ showModalPebble: false });
     }
 
     render() {
-
         return (
             <div>
-                {this.state.Fish ? <AnimationsLayered animations={['happyBackground', 'fish']} /> :
-                    <AnimationsLayered animations={['happyBackground']} />
-                }
-                {this.state.Fish ? this.hideFish() : this.showFish()}
+                <AnimationsLayered animations={['happyBackground']} />
+                <button onClick={this.handleOpenModalPlant} className='clickablePlant'></button>
+                <button onClick={this.handleOpenModalPebble} className='clickablePebble'></button>
+                {this.state.Fish && <div className='anim-2'><button onClick={this.handleOpenModalFish} className='clickableFish'></button>
+                    <Animation animation='fish' /></div>}
+
+                <ReactModal
+                    isOpen={this.state.showModalFish}
+                    ariaHideApp={false}
+                >
+                    <button onClick={this.handleCloseModalFish}>X</button>
+                    <h1>fish modal</h1>
+                </ReactModal>
+
+                <ReactModal
+                    isOpen={this.state.showModalPlant}
+                    ariaHideApp={false}
+                >
+                    <button onClick={this.handleCloseModalPlant}>X</button>
+                    <h1>plant modal</h1>
+                </ReactModal>
+
+                <ReactModal
+                    isOpen={this.state.showModalPebble}
+                    ariaHideApp={false}
+                >
+                    <button onClick={this.handleCloseModalPebble}>X</button>
+                    <h1>pebble modal</h1>
+                </ReactModal>
+
             </div>
         );
     }
