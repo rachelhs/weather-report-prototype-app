05da43209025f4b31f04fc6d8de4cb7d2a4c2e5c
EllieForeman <foreman.eleanor@gmail.com>
2020-09-29 13:38:13 +0100

    fixed routing problm

--- src/components/Home/MoreThanThreeHome.js
+++ src/components/Home/MoreThanThreeHome.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import BackgroundAnimation from '../Animations/BackgroundAnimation'
-import ForegroundAnimation from '../Animations/ForegroundAnimation'
 import { Link } from 'react-router-dom';
+import { AnimationsLayered } from '../SharedComponents/SharedComponents';
 
 
 export class ThreeHomePage extends React.Component {
@@ -10,12 +9,7 @@ export class ThreeHomePage extends React.Component {
         
         return (
             <div className='background-box'>
-                <div className='background-anim'>
-                    <BackgroundAnimation />
-                </div>
-                <div className='foreground-anim'>
-                    <ForegroundAnimation />
-                </div>
+                <AnimationsLayered speeds={[0]} animations={['neutralBackground']} />
                 <div className='info-box-landing'>
                     <h1 className='info-box-text-padding-top'>You have registered how you're feeling more than three times today.</h1>
                     <p>From feedback, we have set a limit on entering this 3 times a day</p>
